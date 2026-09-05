a167cf905285e23969281fd1c5cf08d9d20098a0
Taís Foltran Orsini <86001169+taisforsini@users.noreply.github.com>
2026-09-05 17:10:26 +0000

    feat(DS): add email input

--- src/pages/FomularioDS/index.tsx
+++ src/pages/FomularioDS/index.tsx
@@ -3,15 +3,25 @@ import { Input } from "../../components/Input/Input";
 
 const FormularioDS = () => {
   const [name, setName] = useState<string>();
+  const [email, setEmail] = useState<string>();
+  const [emailErrorMessage, setEmailErrorMessage] = useState<string>();
+
   return (
     <form>
       <h1>Formulário de cadastro com Design System</h1>
       <Input
         label="Nome completo"
-        placeholder="Nome completo"
+        placeholder="Digite seu nome"
         value={name}
         onChange={(e) => setName(e.target.value)}
       />
+      <Input
+        label="Email"
+        placeholder="email@exemplo.com"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      errorMessage={emailErrorMessage}
     </form>
   );
 };
