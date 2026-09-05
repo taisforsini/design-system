ae630eef74557340c48e23a135c5e9f4801ec8ce
Taís Foltran Orsini <86001169+taisforsini@users.noreply.github.com>
2026-09-05 17:48:35 +0000

    feat(PURO): add name input

--- src/pages/FormularioPuro/index.tsx
+++ src/pages/FormularioPuro/index.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import { useState } from "react";
+import "./FormularioPuro.css";
 
 const FormularioPuro = () => {
-  return (
-    <div>FormularioPuro</div>
-  )
-}
+  const [name, setName] = useState<string>("");
 
-export default FormularioPuro
+  return (
+    <form className="form-wrapper">
+      <h1 className="form-title">Formulário de cadastro sem Design System</h1>
+
+      <div className="custom-input">
+        <label className="custom-input-label">Nome completo</label>
+        <input
+          id="nome"
+          className="custom-input-field"
+          type="text"
+          placeholder="Digite seu nome"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </div>
+    </form>
+  );
+};
+
+export default FormularioPuro;
