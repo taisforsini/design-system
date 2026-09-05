c0b00bb2debc822e37fa3acc1516a0b78fa9eafb
Taís Foltran Orsini <86001169+taisforsini@users.noreply.github.com>
2026-09-05 17:08:02 +0000

    feat(DS): add name input and state

--- src/pages/FomularioDS/index.tsx
+++ src/pages/FomularioDS/index.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import { useState } from "react";
+import { Input } from "../../components/Input/Input";
 
 const FormularioDS = () => {
+  const [name, setName] = useState<string>();
   return (
-    <div>FormularioDS</div>
-  )
-}
+    <form>
+      <h1>Formulário de cadastro com Design System</h1>
+      <Input
+        label="Nome completo"
+        placeholder="Nome completo"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+    </form>
+  );
+};
 
-export default FormularioDS
+export default FormularioDS;
