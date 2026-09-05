6eb65f63ea49973f4cbde06343bfb456640ac405
Taís Foltran Orsini <86001169+taisforsini@users.noreply.github.com>
2026-09-05 17:58:45 +0000

    feat(PURO): add email input with error message

--- src/pages/FormularioPuro/index.tsx
+++ src/pages/FormularioPuro/index.tsx
@@ -3,6 +3,8 @@ import "./FormularioPuro.css";
 
 const FormularioPuro = () => {
   const [name, setName] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [emailErrorMessage, setEmailErrorMessage] = useState<string>("");
 
   return (
     <form className="form-wrapper">
@@ -18,6 +20,25 @@ const FormularioPuro = () => {
           value={name}
           onChange={(e) => setName(e.target.value)}
         />
+      </div>
+
+      <div className="custom-input">
+        <label className="custom-input-label">Email</label>
+        <input
+          id="email"
+          className={
+            emailErrorMessage
+              ? "custom-input custom-input-error"
+              : "custom-input-field"
+          }
+          type="email"
+          placeholder="email@exemplo.com"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        {emailErrorMessage && (
+          <span className="custom-error-text">{emailErrorMessage}</span>
+        )}
       </div>
     </form>
   );
